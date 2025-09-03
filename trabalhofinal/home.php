688fd60dc6083df302efc8389645005c04bc792b
Miguel <miguelviniciusbarbosadias@gmail.com>
2025-09-03 08:12:23 -0300

    coloquei links para ir de página em página

--- trabalhofinal/home.php
+++ trabalhofinal/home.php
@@ -223,9 +223,9 @@
   <aside class="sidebar">
     <img src="fundo.png" alt="Logo Sistema">
     <nav class="menu">
-      <a href="#" class="active">🏠 HOME</a>
-      <a href="#">👤 INFORMAÇÕES PESSOAIS</a>
-      <a href="#">📊 RELATÓRIO</a>
+      <a href="home.php" class="active">🏠 HOME</a>
+      <a href="informacoes.php">👤 INFORMAÇÕES PESSOAIS</a>
+      <a href="relatorio.php">📊 RELATÓRIO</a>
       <a href="#">ℹ️ SOBRE</a>
     </nav>
   </aside>
