edc1b9e0b962026ebe08bfdeb7357554c02c56a4
Miguel <miguelviniciusbarbosadias@gmail.com>
2025-09-10 09:05:39 -0300

    index consegue puxar o css

--- trabalhofinal/index.php
+++ trabalhofinal/index.php
@@ -6,7 +6,8 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
- <link rel="stylesheet" href="index.css">
+  <!-- truque do cache busting -->
+  <link rel="stylesheet" href="index.css?v=<?php echo time(); ?>">
   <title>Sistema de Controle de Medicação - Cadastro</title>
 </head>
 <body>
